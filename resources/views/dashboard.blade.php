91c91e22fe743319a4ec5a0adf8f45cf708e6512
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-22 18:13:37 +0200

    Deleted header (not nav-header) and added shadow to nav-header

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,8 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Coders Free
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, porro? Accusamus ratione sed nesciunt libero quisquam! Repudiandae debitis deleniti voluptatum voluptas facilis laboriosam fuga veniam dolores, iste sit soluta quidem.</p>
-                <p>Quam soluta asperiores quis mollitia tempora fugiat, amet odit quia molestiae voluptatem suscipit dolore, rem odio quibusdam tempore. Excepturi sit dolorum hic! Magni laudantium sapiente consectetur tenetur. Molestias, consectetur fugit?</p>
-                <p>Deleniti assumenda iste pariatur ipsa asperiores beatae enim aperiam voluptates architecto! Sequi laboriosam ducimus cumque, quaerat fuga consectetur debitis quam fugit at id libero commodi nihil quae voluptatibus consequatur magnam!</p>
-                <p>Aliquam nisi, incidunt cum iste laudantium sint? Explicabo reiciendis eum praesentium veritatis reprehenderit alias porro est obcaecati iure? Rem consectetur blanditiis iure in a, quam maxime. Voluptatibus ipsa deleniti quis.</p>
-                <p>Culpa similique doloribus alias est? Dolores cum ratione optio nobis asperiores reprehenderit sit, ipsum id assumenda nulla inventore! Mollitia consequuntur fuga nostrum, pariatur fugiat officiis corporis soluta odit? Est, totam!</p>
+                <x-jet-welcome />
             </div>
         </div>
     </div>
