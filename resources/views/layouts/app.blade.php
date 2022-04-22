ff1e06d1585d4cc7f4e361b29740a325fac5e42d
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-22 19:02:15 +0200

    Added route home at routes file to go to welcome page. Used app-layout component at home page. Changed navigation menu livewire component to only show profile options when user is authenticated, if not it will show login and register options. Logo now sends to home page. Added margin-top to first alert at welcome page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,8 @@
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="https://cdn.tailwindcss.com" defer></script>
+
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
